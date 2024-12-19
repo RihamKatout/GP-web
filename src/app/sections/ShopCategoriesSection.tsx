e3412229cf253a097d1e8c08dfd6298186490ab4
Riham Katout <rihamkatm@gmail.com>
2024-12-20 00:21:40 +0200

    chore: add a basic loader

--- src/app/sections/ShopCategoriesSection.tsx
+++ src/app/sections/ShopCategoriesSection.tsx
@@ -3,14 +3,13 @@ import { getShopCategoriesApi } from "../api/shop";
 import { CategoryCard } from "../components/shared";
 import { ShopCategory } from "../types";
 import { CategoriesGrid } from "../styles";
-
+import { Loader } from "../components/shared";
 export const ShopCategoriesSection = () => {
   const {
     data: categories,
     isLoading,
     error,
   } = useQuery(["categories"], getShopCategoriesApi);
-  if (isLoading) return <p>Loading categories...</p>;
   if (error instanceof Error) return <p>Error: {error.message}</p>;
 
   return (
@@ -24,20 +23,28 @@ export const ShopCategoriesSection = () => {
       }}
     >
       <h1
-        style={{ fontFamily: "DynaPuff", fontWeight: "400", fontSize: "4rem" }}
+        style={{
+          fontFamily: "DynaPuff",
+          fontWeight: "400",
+          fontSize: "3.5rem",
+        }}
       >
         Shop Categories
       </h1>
-      <CategoriesGrid>
-        {categories?.data.map((category: ShopCategory) => (
-          <CategoryCard
-            key={category.id}
-            title={category.categoryName}
-            id={category.id}
-            imageURL={category.imageURL}
-          />
-        ))}
-      </CategoriesGrid>
+      {isLoading ? (
+        <Loader type="bouncing"/>
+      ) : (
+        <CategoriesGrid>
+          {categories?.data.map((category: ShopCategory) => (
+            <CategoryCard
+              key={category.id}
+              title={category.categoryName}
+              id={category.id}
+              imageURL={category.imageURL}
+            />
+          ))}
+        </CategoriesGrid>
+      )}
     </div>
   );
 };
